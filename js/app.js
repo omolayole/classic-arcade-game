c76a1126974faed81aedfd318a3de22ee3300996
omolayole <omolayoleogunyemi@gmail.com>
2018-10-16 11:43:31 +0100

    update the conditional statement for checking collision between the player and the enemy

--- js/app.js
+++ js/app.js
@@ -68,7 +68,8 @@ Player.prototype.update = function() {
         // when player x and y collide with enemy?
         // decrement player's score
         // and reset player to initial position
-        if (this.y === enemy.y && (enemy.x + enemy.step/2 > this.x && enemy.x < this.x + this.step/2)) {
+        if (this.y === enemy.y && (enemy.x + enemy.step/1.5 > this.x && enemy.x < this.x + this.step/1.5)) {
+            //alert('stop')
             scoreNumber.textContent = this.num--;
             this.resetPlayer();
         }
